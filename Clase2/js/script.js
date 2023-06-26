0e2b3109d0f2372b37186723fded4acc257d6a82
Lucas Herrero <lucas.herrero@piano.io>
2023-06-26 19:09:24 -0300

    started Clase2

---
+++ Clase2/js/script.js
@@ -0,0 +1,20 @@
+// let edadMinima = 18
+//
+//
+// if (edadMinima < 18) {
+//     console.warn('Volve a tu casa')
+// } else if (edadMinima >= 18) {
+//     console.log('Puede entrar a la disco')
+// } else {
+//     console.log('Algo no esta bien ' + edadMinima)
+// }
+
+
+lenguajeRequerido = 'JS'
+let idiomaRequerido = 'frances'
+
+if ((lenguajeRequerido = 'JS' )&&(idiomaRequerido = 'frances')){
+    console.log('Puede Acceder al puesto')
+} else{
+    console.warn('Debe cumplir con todos los requisitos.')
+}
